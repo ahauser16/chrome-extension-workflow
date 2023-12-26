51104640bb51c31df84bfe963f93d6cd06322af4
Arthur Hauser <arthur.hauser@gmail.com>
2023-12-26 18:48:33 -0500

    back to drawing board

--- E-notary-services/script.js
+++ E-notary-services/script.js
@@ -1,14 +1,29 @@
-// top level await is available in ES modules loaded from script tags
+
+//OVERVIEW: In summary, the script waits for the user to click a button, and upon clicking, it opens a side panel in the context of the currently active tab, displaying the content from 'sidepanel-tab.html'. This functionality is typically part of a Chrome extension that aims to enhance user interaction by providing additional information or controls in a side panel.
+
+//Step One: Query the Active Tab
+// this uses the `chrome.tabs.query` method to find the currently active tab in the last focused window.
 const [tab] = await chrome.tabs.query({
+  //`active: true` ensures that only the active tab in each window is considered.
   active: true,
+  //`lastFocusedWindow: true` restricts the search to the last window that was focused.
   lastFocusedWindow: true
 });
+// The result is an array of tabs, but since only one tab is expected (the active tab in the focused window), array destructuring is used to get the first element of the array directly (`const [tab] = ...`).
 
+//Step Two: Retrieve the Tab ID
 const tabId = tab.id;
-const button = document.getElementById('openSidePanel');
-button.addEventListener('click', async () => {
-  await chrome.sidePanel.open({ tabId });
-  await chrome.sidePanel.setOptions({
+//this extracts the ID of the active tab and stores it in the variable `tabId`.
+
+//Step Three: Get the Button Element and Add Event Listener
+const button = document.getElementById('openSidePanel'); //this line retrieves the button element from the DOM using its ID ('openSidePanel').
+button.addEventListener('click', async () => {//An event listener is added to this button to respond to click events.
+  
+  //Step Four: Event Listener Functionality
+  //Inside the event listener, when the button is clicked, the following actions are performed
+  await chrome.sidePanel.open({ tabId });//This line uses the chrome.sidePanel.open method to open the side panel associated with the current tab (tabId).
+
+  await chrome.sidePanel.setOptions({//This line sets options for the side panel of the current tab. It specifies the path to the HTML file ('sidepanel-tab.html') that should be displayed in the side panel and ensures that the side panel is enabled (enabled: true).
     tabId,
     path: 'sidepanel-tab.html',
     enabled: true
