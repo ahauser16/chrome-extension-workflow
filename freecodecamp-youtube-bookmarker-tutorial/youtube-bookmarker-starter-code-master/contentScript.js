e8b841321c5bf972f0e20f684099aa89507f7b16
Arthur Hauser <arthur.hauser@gmail.com>
2023-12-27 18:17:28 -0500

    add more notes on youtube tutorial prior to UI section

--- freecodecamp-youtube-bookmarker-tutorial/youtube-bookmarker-starter-code-master/contentScript.js
+++ freecodecamp-youtube-bookmarker-tutorial/youtube-bookmarker-starter-code-master/contentScript.js
@@ -15,8 +15,18 @@
         }
     });
 
-    const newVideoLoaded = () => {
+    //this function appears in two places on this page.
+    const fetchBookmarks = () => {
+        return new Promise((resolve) => {
+            chrome.storage.sync.get([currentVideo], (obj) => {
+                resolve(obj[currentVideo] ? JSON.parse(obj[currentVideo]) : []);
+            });
+        });
+    };
+
+    const newVideoLoaded = async () => {
         const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
+        currentVideoBookmarks = await fetchBookmarks();
         console.log(bookmarkBtnExists);
 
         //if the Bookmark Button does not exist then we want to add it to every Youtube video website that we go to.  The code below creates the button.
@@ -26,17 +36,18 @@
             bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
             bookmarkBtn.className = "ytp-button " + "bookmark-btn";
             bookmarkBtn.title = "Click to bookmark current timestamp";
-            
+
             youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
             youtubePlayer = document.getElementsByClassName("video-stream")[0];
-            
+
             //the video uses `appendChild` and not `append`.  at the end of the tutorial if the extension does not work revisit this point.
-            youtubeLeftControls.append(bookmarkBtn);
+            youtubeLeftControls.appendChild(bookmarkBtn);
+
             bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
         }
     }
 
-    const addNewBookmarkEventHandler = () => {
+    const addNewBookmarkEventHandler = async () => {
         const currentTime = youtubePlayer.currentTime;
         const newBookmark = {
             time: currentTime,
@@ -44,11 +55,14 @@
         };
         console.log(newBookmark);
 
+        currentVideoBookmarks = await fetchBookmarks();
+
         chrome.storage.sync.set({
             [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time))
         });
     }
 
+    //**see note in README**
     newVideoLoaded();
 })();
 
--- freecodecamp-youtube-bookmarker-tutorial/youtube-bookmarker-starter-code-master/popup.js
+++ freecodecamp-youtube-bookmarker-tutorial/youtube-bookmarker-starter-code-master/popup.js
@@ -1,12 +1,32 @@
+import { getActiveTabURL } from "./utils";
+
 // adding a new bookmark row to the popup
-const addNewBookmark = () => {};
+const addNewBookmark = () => { };
 
-const viewBookmarks = () => {};
+const viewBookmarks = () => { };
 
-const onPlay = e => {};
+const onPlay = e => { };
 
-const onDelete = e => {};
+const onDelete = e => { };
 
-const setBookmarkAttributes =  () => {};
+const setBookmarkAttributes = () => { };
 
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", async () => {
+    const activeTab = await getActiveTabURL();
+    const queryParameters = activeTab.url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters);
+
+    const currentVideo = urlParameters.get("v");
+
+    if (activeTab.url.includes("youtube.com/watch") && currentVideo) {
+        chrome.storage.sync.get([currentVideo], (data) => {
+            const currentVideoBookmarks = data[currentVideo] ? JSON.parse(data[currentVideo]) : [];
+
+            viewBookmarks(currentVideoBookmarks);
+        });
+    } else {  //this else condition occurs when the currentVideo variable returns a falsey value or when we are not on a Youtube video webpage.
+        const container = document.getElementsByClassName("container")[0];
+
+        container.innerHTML = '<div class="title">This is not a youtube video page.</div>';
+    }
+});
