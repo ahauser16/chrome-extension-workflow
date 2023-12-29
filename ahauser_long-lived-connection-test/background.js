a09fe1b79f55749ae1e04980dab7b8c4b1f3d51f
Arthur Hauser <arthur.hauser@gmail.com>
2023-12-29 17:10:04 -0500

    update testing & debugging code before test-deployment

--- ahauser_long-lived-connection-test/background.js
+++ ahauser_long-lived-connection-test/background.js
@@ -1,12 +1,16 @@
 
 //I. Listening for Connections
 chrome.runtime.onConnect.addListener((port) => {//<-- line A 
+  console.log("Connection established with content script"); // Log when a connection is established
+
 
   //II. Establishing the Connection
   console.assert(port.name === "meetConnection");//<-- line B 
 
   //III. Communication
   port.onMessage.addListener((msg) => {//<-- line C 
+    console.log("Message received from content script:", msg); // Log received messages
+
     if (msg.greeting === "hello") {
       console.log("Received hello from content script");
       port.postMessage({ greeting: "hi there!" });
--- ahauser_long-lived-connection-test/contentScript.js
+++ ahauser_long-lived-connection-test/contentScript.js
@@ -5,8 +5,11 @@ const port = chrome.runtime.connect({ name: "meetConnection" });//<--Line A
 port.postMessage({ greeting: "hello" });//<--Line B
 
 port.onMessage.addListener((msg) => {//<--Line C
+  console.log("Message received from background script:", msg); // Log received messages
+
   if (msg.greeting === "hi there!") {//<--Line D
-    console.log("Received greeting from background script");
-    // Additional logic here
+    console.log("Received 'hi there!' from background script");
+    // Example interaction: Change the background color of the Google Meet page
+    document.body.style.backgroundColor = 'lightblue';
   }
 });
